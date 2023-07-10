0cb47aa372e4247ad2d19e8b41f14e6a4e5efc67
WrathXL <christianrd96@gmail.com>
2023-07-09 23:07:03 -0400

    reorganized page

---
+++ src/pages/CheckoutPage.vue
@@ -0,0 +1,17 @@
+<template>
+  <div>
+    <div
+      class="row justify-center q-mt-xl fit text-h2 text-primary text-center"
+    >
+      Oh no! We run out of tickets.
+    </div>
+    <div class="text-black row justify-center q-mt-md">
+      Please check back later.
+    </div>
+  </div>
+</template>
+
+<script setup>
+import { onMounted } from "vue";
+import { useCatalogScript } from "../tools";
+</script>
